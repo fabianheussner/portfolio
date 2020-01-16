c283c220cae141de55bfeb28b48aaa08fc590478
fhdesign <fhdesign@fhdesigns-iMac.local>
2020-01-16 20:44:10 +0100

    CSS Module Sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { Link } from "gatsby"
 
-import styles from './sidebar.scss'
+import styles from './sidebar.module.scss'
 import myLogo from '../../images/logo-dark.svg'
 
 const Sidebar = props => {
-    let drawerClasses = 'sidebar';
-    if (props.show) {
-        drawerClasses = 'sidebar open';
-    }
+    // let drawerClasses = 'sidebar';
+    // if (props.show) {
+    //     drawerClasses = 'sidebar open';
+    // }
 
     return (
-        // <nav className={`${styles.sidebar} ${drawerClasses}`}>
-        <nav className={drawerClasses}>
+        <nav className={`${styles.sidebar} ${props.show ? styles.open : ''}`}>
+        {/* <nav className={drawerClasses}> */}
             {/* <div className="sidebar__logo">
                 <Link to="/"><img src={myLogo} alt="Logo Fabian Heußner" /></Link>
             </div> */}
 
-            <div className="internallinks">
+            <div className={styles.internallinks}>
                 <ul>
                     <li><Link to="/work/">work</Link></li>
                     <li><Link to="/about/">that's me</Link></li>
                     <li><Link to="/contact/">contact</Link></li>
                 </ul>
             </div>
-            <div className="externallinks">
+            <div className={styles.externallinks}>
                 <ul>
                     <li><a href="">Instagram</a></li>
                     <li><a href="">Dribbble</a></li>
