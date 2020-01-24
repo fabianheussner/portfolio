5093f99c84b7eb24481791745eafa7595114f3eb
fhdesign <fhdesign@fhdesigns-iMac.local>
2020-01-24 22:34:12 +0100

    Removed FullpageJS - CoreJS Update - npm Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import StageHome from "../components/stagehome"
 
+
 import "./mystyles.scss"
 import ProjectHome from "../components/project/projecthome"
 
@@ -19,6 +20,7 @@ const IndexPage = () => {
       projecttitle="lightelligence docs" 
       subtitle="Lorem ipsum dolor sit amet, consectetuer adipiscing elit aenean commodo ligula." 
       tags="UI UX Concept Illustration" />
+
     
 
     </Layout>
