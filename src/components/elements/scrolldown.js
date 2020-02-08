4d6baf30e8c1722dd05ab3b3835da81a1d3dc04f
fhdesign <fhdesign@fhdesigns-iMac.local>
2020-02-08 23:41:03 +0100

    Scroll Down Offset Animation

--- src/components/elements/scrolldown.js
+++ src/components/elements/scrolldown.js
@@ -1,22 +1,70 @@
-import React from "react"
+import React from 'react'
 import styles from './scrolldown.module.scss'
-import Fade from 'react-reveal/Fade'
-// import Reveal from 'react-reveal/Reveal'
+import Reveal from 'react-reveal/Reveal'
 
 
-const ScrollDown = () => (
-    <div className={styles.scrolldown}>
-        <Fade>
-        {/* <Reveal effect="rotateIn" effectOut="rotateOut" delayOut={100}> */}
-            <h5 className={styles.label}>Latest projects</h5>
-            <div className={styles.slider}>
-                <div className={styles.line}></div>
-                <div className={`${styles.subline} ${styles.inc}`}></div>
-                <div className={`${styles.subline} ${styles.dec}`}></div>
+
+
+
+class ScrollDown extends React.Component  {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            is_visible: true
+        };
+    }
+    
+    componentDidMount() {
+        var scrollComponent = this;
+        document.addEventListener("scroll", function(e) {
+            scrollComponent.toggleVisibility();
+        });
+    }
+
+    
+    // toggleVisibility() {
+    //     if (window.pageYOffset > 50) {
+    //         this.setState({
+    //             is_visible: false
+    //         });
+    //     } else {
+    //         this.setState({
+    //             is_visible: true
+    //         });
+    //     }
+    // }
+
+    // My Code - Add / Remove Class Start
+    toggleVisibility() {
+        if (window.pageYOffset > 50) {
+            document.getElementById('scrolldown-test').classList.add('test');
+        } else {
+            document.getElementById('scrolldown-test').classList.remove('test');
+        }
+    }
+    // My Code - Add / Remove Class End
+    
+    render() {
+        const { is_visible } = this.state;
+        return (
+            <div className="scrolldown__wrapper" id="scrolldown-test">
+                {is_visible && (
+                    <div className={styles.scrolldown}>
+                        <Reveal effect="fadeInUp" effectOut="pulse">
+                            <h5 className={styles.label}>Latest projects</h5>
+                            <div className={styles.slider}>
+                                <div className={styles.line}></div>
+                                <div className={`${styles.subline} ${styles.inc}`}></div>
+                                <div className={`${styles.subline} ${styles.dec}`}></div>
+                            </div>
+                        </Reveal>
+                    </div>
+                )}
             </div>
-        {/* </Reveal> */}
-        </Fade>
-    </div>
-)
+        );
+    }
+}
+    
 
-export default ScrollDown
+export default ScrollDown;
