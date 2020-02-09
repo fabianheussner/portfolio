686aa5df5e11c01a553d95bd371cdf6dd8433ad2
fhdesign <fhdesign@fhdesigns-iMac.local>
2020-02-09 16:22:01 +0100

    Clean Up Scrolldown Trigger

--- src/components/elements/scrolldown.js
+++ src/components/elements/scrolldown.js
@@ -3,17 +3,7 @@ import styles from './scrolldown.module.scss'
 import Reveal from 'react-reveal/Reveal'
 
 
-
-
-
 class ScrollDown extends React.Component  {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            is_visible: true
-        };
-    }
     
     componentDidMount() {
         var scrollComponent = this;
@@ -22,34 +12,17 @@ class ScrollDown extends React.Component  {
         });
     }
 
-    
-    // toggleVisibility() {
-    //     if (window.pageYOffset > 50) {
-    //         this.setState({
-    //             is_visible: false
-    //         });
-    //     } else {
-    //         this.setState({
-    //             is_visible: true
-    //         });
-    //     }
-    // }
-
-    // My Code - Add / Remove Class Start
     toggleVisibility() {
-        if (window.pageYOffset > 50) {
+        if (window.pageYOffset > 100) {
             document.getElementById('scrolldown-test').classList.add('test');
         } else {
             document.getElementById('scrolldown-test').classList.remove('test');
         }
     }
-    // My Code - Add / Remove Class End
     
     render() {
-        const { is_visible } = this.state;
         return (
             <div className="scrolldown__wrapper" id="scrolldown-test">
-                {is_visible && (
                     <div className={styles.scrolldown}>
                         <Reveal effect="fadeInUp" effectOut="pulse">
                             <h5 className={styles.label}>Latest projects</h5>
@@ -60,7 +33,6 @@ class ScrollDown extends React.Component  {
                             </div>
                         </Reveal>
                     </div>
-                )}
             </div>
         );
     }
